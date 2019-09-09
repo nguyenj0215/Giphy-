4936e540b52dfd02d2c1407a14ba4c68e9a78d5c
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-09-08 21:31:54 -0700

    fixed issue with corb and made gifs pause and start on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,62 +1,101 @@
 
-var giphyArray = ["dog", "cat"];
+$(document).ready(function () {
 
-function displayGifs() {
+    //Initial array of buttons to be displayed
+    var giphyArray = ["dog", "cat", "owl", "bear"];
 
-    var gif = $(this).attr("data-name");
+    function displayGifs() {
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=xhOeDtimrOu6oqIq9pKAlNILZZqFHYAL&q=" + gif + 
-    "&limit=10&offset=0&rating=G&lang=en"
+        //Button reference
+        var gif = $(this).attr("data-name");
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response)
+        //Giphy API ajac reference
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=xhOeDtimrOu6oqIq9pKAlNILZZqFHYAL&q=" + gif +
+            "&limit=10&offset=0&rating=G&lang=en";
 
-      var newDiv = $("<div>")
+        $.ajax({
+            url: queryURL,
+            method: "GET",
 
-      var newRated = response.data[0].rating
-      var ratedElement = $("<div>").text("Rated " + newRated)
-      newDiv.append(ratedElement)
+        }).then(function (response) {
 
-        var newImg = response.data[0].bitly_gif_url
-        var imgElement = $("<img>").attr("src", newImg)
-        newDiv.append(imgElement)
+            // Loop through response which is limited by limit = "10" in link
+            for (var j = 0; j < response.data.length; j++) {
 
-      $("#movies-view").prepend(newDiv)
+                var newDiv = $("<div>")
+
+                //Rating display, maybe change to title
+                var newRated = response.data[j].rating
+                var ratedElement = $("<div>").text("Rated " + newRated)
+                newDiv.append(ratedElement)
+
+                //Gif display
+                var newImg = response.data[j].images.fixed_height_still.url
+                var imgElement = $("<img>").attr("src", newImg)
+                //Data still reference
+                imgElement.attr("data-still", response.data[j].images.fixed_height_still.url)
+                //Data animate reference
+                imgElement.attr("data-animate", response.data[j].images.fixed_height.url)
+                //Initial state 
+                imgElement.attr("data-state", "paused")
+                //Class for buttons to trigger state change on click
+                imgElement.addClass("gifState")
+                newDiv.append(imgElement)
+
+                $("#gifs-view").prepend(newDiv)
+
+            }
+        });
+
+    }
+
+    function renderButtons() {
+
+        $("#buttons-view").empty();
+
+        for (var i = 0; i < giphyArray.length; i++) {
+
+            var addButton = $("<button>");
+
+            addButton.addClass("gif");
+
+            addButton.attr("data-name", giphyArray[i]);
+
+            addButton.text(giphyArray[i]);
+
+            $("#buttons-view").append(addButton);
+        }
+    }
+
+    function playPauseGif() {
+
+        var currentGifState = $(this).attr("data-state")
+
+        if (currentGifState == "paused") {
+        //If gif is paused change src link to animated gif and change data state value to active
+            $(this).attr("src", $(this).attr("data-animate"))
+            $(this).attr("data-state", "active")
+        }
+        else {
+            $(this).attr("src", $(this).attr("data-still"))
+            $(this).attr("data-state", "paused")
+        }
+    }
+
+    $("#add-gifs").on("click", function (event) {
+        event.preventDefault();
+
+        var gif = $("#gif-input").val().trim();
+
+        giphyArray.push(gif);
+
+        renderButtons();
     });
 
-  }
+    $(document).on("click", ".gif", displayGifs);
 
-function renderButtons() {
-
-    $("#buttons-view").empty();
-
-    for (var i = 0; i < giphyArray.length; i++) {
-
-      var a = $("<button>");
-
-      a.addClass("gif");
-
-      a.attr("data-name", giphyArray[i]);
-
-      a.text(giphyArray[i]);
-
-      $("#buttons-view").append(a);
-    }
-  }
-
-  $("#add-movie").on("click", function(event) {
-    event.preventDefault();
-
-    var gif = $("#movie-input").val().trim();
-
-    giphyArray.push(gif);
+    $(document).on("click", ".gifState", playPauseGif);
 
     renderButtons();
-  });
 
-  $(document).on("click", ".gif", displayGifs);
-
-  renderButtons();
+})
